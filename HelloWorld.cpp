6e10fbd7e03e857b7c9f1eb624e5558e7fa702c5
kek <astanaevv@bk.ru>
2021-08-17 01:59:32 +0700

    Add project files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+//Вызывает функцию std::cout
+void print()
+{
+    //Вызываем оператор << на объект типа std::cout
+    std::cout << "Hello SkillBox!\n";//This function prints
+}
+
+/*
+* Присваиваем значение переменным и печатаем Hello Wordl!
+*/
+
+int main()
+{
+    int x=100;
+    int y=x+100;
+
+    int b=0;
+    b = b + 2;
+
+
+    //Показывает пример инициализации 
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+
+    int mult = x * y;
+    int random=1002;
+
+
+
+    std::cout << random;
+}
